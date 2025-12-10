f290b032d873b9d3e67790829614b21a6c229973
secretindex <caio.smelo04@gmail.com>
2025-12-10 11:22:10 -0300

    fixed wrong text for jardim prev

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,10 +1,7 @@
 const upperText: string = `Os servidores públicos ativos, aposentados e pensionistas deverão apresentar, obrigatoriamente, os seguintes documentos ORIGINAIS:
 `
 const bottomText: string = `
-Assine as declarações e termos abaixo a punho. Também aceitamos assinatura digital com gov.br.
-
-Para ter acesso às declarações e termos citados acesse o decreto pelo link abaixo:
-https://www.aracoiabaprev.pe.gov.br/wp-content/uploads/2025/08/DECRETO-MUNICIPAL-034-2025-CENSO-PREVIDENCIARIO.pdf
+Assine as declarações e termos a punho ou assinatura digital com gov.br.
 `;
 
 export default { upperText, bottomText }
